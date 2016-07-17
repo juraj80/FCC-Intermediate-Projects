ba8763c5c8a9c531acee780df536fd4090f151e7
Juraj <juraj.klucka@gmail.com>
2016-07-17 18:16:54 +0200

    Create 03_wikipedia_viewer.js
    wikedia viewer

---
+++ 03-Wikipedia_Viewer/03_wikipedia_viewer.js
@@ -0,0 +1,45 @@
+<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Rancho' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+
+<div class="container">
+  <!--
+  <div class="Jumbotron text-center ">
+    <h1 class="animated bounceInDown">Search </h1>
+  </div> -->
+  <div class="row">
+    <div class="col-xs-4 col-xs-offset-4 smallpad" <form action="Search Wiki">
+      <div class="row">
+        <div class="col-xs-10 smallpad">
+          <input id="searchEdit" class="form-control animated bounceInLeft" type="text" placeholder="What are you looking for?" required>
+        </div>
+        <div class="col-xs-2 smallpad">
+          <button type="button" class="btn btn-block animated bounceInRight " aria-label="Search" id="searchButton">       
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+             </button>
+        </div>
+
+        <div class="col-xs-12 bgimage  animated fadeInUpBig" id="imageBG">
+          <div class="row  resultWindow">
+            <div class="col-xs-12 animated" id="outputArea">
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <!-- row form -->
+      </form>
+
+    </div>
+    <!-- central column -->
+  </div>
+  <!-- row -->
+
+</div>
+<!-- container -->
+<!--
+
+-->
